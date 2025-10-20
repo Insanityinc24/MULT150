17b364560799bddddc172da2074c3d65d4f04515
Insanityinc24 <spongebong24@gmail.com>
2025-10-19 19:39:43 -0700

    Complete Hour7 PT1

---
+++ ASouth_Hour7_P1/Assets/Scripts/HelloWorldScript.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+public class HelloWorldScript : MonoBehaviour
+{
+    //Use this for intialization
+    void Start()
+    {
+        print("Hello World");
+}
+    //Update is called once per frame
+    void Update()
+    {
+    }
+}
