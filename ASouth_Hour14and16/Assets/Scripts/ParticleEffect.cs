0d3fcb11ef6b294286c23ec5686152aa846b5307
Insanityinc24 <spongebong24@gmail.com>
2025-11-24 00:00:03 -0800

    Finished Hour14and16

---
+++ ASouth_Hour14and16/Assets/Scripts/ParticleEffect.cs
@@ -0,0 +1,6 @@
+/* 1. Made a curve on the Start Speed
+ * 2. Change the shape to a Donut
+ * 3. Change Emission rate over time to 13
+ * 4. Change rotation by speed angular velocity to 75
+ * 5. Change noise strength to 3
+ */
---
+++ ASouth_Hour14and16/Assets/Scripts/UserInterface.cs
@@ -0,0 +1,3 @@
+/* Made my UI into a title screen for a made up game named Monkey Business. 
+ * Changes I made were I change background image to a chimp in a suit, made the background color red for grungy look, made the title of the a gradient yellow/brown to give it that banana look to the word, made a cool opening sentence to make the game more dramatic and made the start point labeled "Go Bananas" and it turns to red and spells out a sound a chimp makes.
+*/
