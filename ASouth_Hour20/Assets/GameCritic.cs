49600955366794a6daa9de89f5bba6ff6f98815f
Insanityinc24 <spongebong24@gmail.com>
2025-12-15 01:31:16 -0800

    Completed Hour 20

---
+++ ASouth_Hour20/Assets/GameCritic.cs
@@ -0,0 +1,44 @@
+/*
+ 
+	
+		
+  _______      ___      .___  ___.  _______   ______ .______       __  .___________. __    ______ 
+ /  _____|    /   \     |   \/   | |   ____| /      ||   _  \     |  | |           ||  |  /      |
+|  |  __     /  ^  \    |  \  /  | |  |__   |  ,----'|  |_)  |    |  | `---|  |----`|  | |  ,----'
+|  | |_ |   /  /_\  \   |  |\/|  | |   __|  |  |     |      /     |  |     |  |     |  | |  |     
+|  |__| |  /  _____  \  |  |  |  | |  |____ |  `----.|  |\  \----.|  |     |  |     |  | |  `----.
+ \______| /__/     \__\ |__|  |__| |_______| \______|| _| `._____||__|     |__|     |__|  \______|
+                                                                                                  
+
+
+	
+
+
+1. Is the game too easy or hard? 
+Not really I found it very easy to keep the game going.
+
+2. What changes would you do to make it easier or harder? 
+Maybe make more of a penalty if you hit the obstacle to slow you down longer or as you progress make the penalty much larger depending on how much time has elapsed.
+
+3. What would make the game more appealing?
+I think cop out answer is always sound and visuals for me but gameplay wise I would add a little story to it and just a reason why you are running the first place or simply a title screen.
+
+4. What parts of the game are fun? 
+I really like the based idea of this game of just running and surviving I think the overall concept of surviving is funny and could be addicting.
+
+5. What parts of the game are tedious?
+Not really much is tedious but maybe size and brightness of the object are distracting for me and maybe I would change the intensity of it to be able to see the game better.
+
+Obstacle Changes
+Make it into a flying astroid with flames.
+https://www.istockphoto.com/vector/moon-surface-seamless-gm164439166-16646518
+https://texturelabs.org/textures/atmosphere_121/
+
+Powerup Changes
+Make it into a star bit like stars like from Super Mario Galaxy.
+https://stock.adobe.com/search?k=glossy+plastic+texture&asset_id=1034406129
+https://stock.adobe.com/search?k=glitter+texture&asset_id=236041125
+There is going to be a lot of Starbit Prefabs I was having trouble getting it to work and it only worked if I had mutiple Prefabs.
+The particles for the Powerup changes were made to make it more falling star like.
+
+*/
